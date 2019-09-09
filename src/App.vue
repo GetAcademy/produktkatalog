a8e3522accf29e432817d1beb330b17cecf4e1e2
GET Academy <terje@kolderup.net>
2019-09-09 15:10:42 +0200

    x

---
+++ src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      data: {
+        kategorier: ["kniv", "skje", "gaffel"],
+        produkter: [
+          { navn: "Gullskje 22cm", pris: 990, kategori: "skje" },
+          { navn: "Sølvskje 22cm", pris: 190, kategori: "skje" },
+          { navn: "Stålskje 22cm", pris: 19, kategori: "skje" },
+          { navn: "Gullkniv 22cm", pris: 990, kategori: "kniv" },
+          { navn: "Sølvkniv 22cm", pris: 190, kategori: "kniv" },
+          { navn: "Stålkniv 22cm", pris: 19, kategori: "kniv" },
+          { navn: "Gullgaffel 22cm", pris: 990, kategori: "gaffel" },
+          { navn: "Sølvgaffel 22cm", pris: 190, kategori: "gaffel" },
+          { navn: "Stålgaffel 22cm", pris: 19, kategori: "gaffel" }
+        ],
+        tekster: {
+          overskrift: 'Bestikkshoppen',
+          velkommen: "Velkommen til Bestikkshoppen AS. Vi håper du får mange flotte handleopplevelser!",
+          kontaktOss: {
+            navn: "Bestikkshoppen AS",
+            adresse: "Melkeveien 1",
+            postnr: "0123",
+            posted: "Oslo",
+            epost: "post@bestikkshoppen.no",
+            web: "www.bestikkshoppen.no"
+          }
+        }
+      },
+      metadata: {
+        sider: {
+          forside: { mal: "infoSide" },
+          kategoriListe: { mal: "listeSide" },
+          produktListe: { mal: "listeSide" },
+          enkeltProdukt: { mal: "infoSide" },
+          kontaktOss: { mal: "infoSide" }
+        },
+        maler: {
+          infoSide: {
+            komponenter: ["overskrift", "skjema-visning"],
+            layout: {
+              rader: "200px auto",
+              kolonner: "auto",
+              mal: ["overskrift", "skjema-visning"]
+            }
+          },
+          listeSide: {
+            komponenter: ["overskrift", "liste-visning"],
+            layout: {
+              rader: "200px auto",
+              kolonner: "auto",
+              mal: ["overskrift", "skjema-visning"]
+            }
+          }
+        }
+      }
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <skjema-visning :data="person" :metadata="metadata" ></skjema-visning>
+  </div>
+</template>
+
+<script>
+import SkjemaVisning from './SkjemaVisning';
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      person: {navn: 'Terje Kolderup', fødselsår: 1975},
+      metadata: {
+        overskrift: 'Hovedperson',
+        felt: [
+          { visningsnavn: 'Navn', navn: 'navn' },
+          { visningsnavn: 'Fødselsår', navn: 'fødselsår' },
+        ]
+      }
+    }
+  },
+  components: {SkjemaVisning}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
